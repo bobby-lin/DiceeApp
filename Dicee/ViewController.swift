f0875bebb136d00ed42b605dacac8ecb613010f9
bobby-lin <mr.linjiahao@gmail.com>
2019-08-27 21:37:44 +0800

    Initial Commit

---
+++ Dicee/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dicee
+//
+//  Created by gamma on 27/8/19.
+//  Copyright © 2019 Demo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
